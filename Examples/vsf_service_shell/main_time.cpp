c274adb00139641d99b847c964eaa189501782e9
Little-Red-Cap <little-red-cap@foxmail.com>
2026-02-15 03:31:18 +0800

    Update main_time.cpp

--- Examples/vsf_service_shell/main_time.cpp
+++ Examples/vsf_service_shell/main_time.cpp
@@ -17,12 +17,12 @@ struct DummyDelay {
     static void delay_ms(hal::tick_t) noexcept {}
 };
 
-static util::usize console_write(shell::Buffer buf) noexcept {
+static util::usize console_write(void*, shell::Buffer buf) noexcept {
     return std::fwrite(buf.data, 1, buf.size, stdout);
 }
 
 int main() {
-    shell::Console con{&console_write};
+    shell::Console con = shell::make_console(&console_write);
     auto now = shell::TimeApi<DummyTime>::now();
     (void)now;
     shell::DelayApi<DummyDelay>::delay_ms(1);

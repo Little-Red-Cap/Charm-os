2416cfb4085cb8125cd56417191f174c2c61c71a
Little-Red-Cap <little-red-cap@foxmail.com>
2026-02-14 14:20:04 +0800

    Create main_module.cpp

---
+++ Examples/vsf_service_shell/main_module.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+
+import module_core;
+import module_loader;
+import module_link;
+
+int main() {
+    modulex::ImageHeader img{};
+    auto loaded = modulex::Loader::load(&img);
+    auto linked = modulex::Linker::relocate(&img, 0);
+    std::printf("[module_demo] load=%d link=%d\n", loaded.ok ? 1 : 0, linked ? 1 : 0);
+    return 0;
+}

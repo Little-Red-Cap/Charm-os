a407dc1db7318bc3e3f026aef7a13e62ec60f982
Little-Red-Cap <little-red-cap@foxmail.com>
2026-04-17 00:10:12 +0800

    继续收敛 listener 和 error/reset 示例 facade 用法

--- Examples/io/net/reactor_service_typed_error_smoke/main.cpp
+++ Examples/io/net/reactor_service_typed_error_smoke/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <utility>
 
 import charm.net;
 import net.backend.stub;
@@ -76,14 +77,19 @@ int main() {
     net::TcpClient client{};
     net::TcpClient server{};
 
-    if (!listener.listen(stack, net::Endpoint::ipv4_loopback(30208), 2)) {
+    auto listening = net::TcpListener::listening_loopback(stack, 30208, 2);
+    if (!listening) {
         std::fputs("reactor service typed error listen failed\n", stderr);
         return 1;
     }
-    if (!client.connect(stack, net::Endpoint::ipv4_loopback(30208))) {
+    listener = std::move(listening.value());
+
+    auto connected = net::TcpClient::connected_loopback(stack, 30208);
+    if (!connected) {
         std::fputs("reactor service typed error connect failed\n", stderr);
         return 2;
     }
+    client = std::move(connected.value());
     if (!listener.accept(server, nullptr)) {
         std::fputs("reactor service typed error accept failed\n", stderr);
         return 3;
